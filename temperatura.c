0ac6c9234964aeee801e316146c9e39119d08e19
Carlos <cdiezg07@estudiantes.unileon.es>
2019-02-14 14:09:10 +0100

    Convertir de grados Celsius a Kelvin

---
+++ temperatura.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int cent,kelvin;
+
+printf("Introduce una temperatura en grados centigrados\n");
+scanf("%d",&cent);
+
+kelvin=cent+273;
+
+printf("La temperatura en kelvin es " "%d\n",kelvin);
+
+
+
+return 0;
+}
