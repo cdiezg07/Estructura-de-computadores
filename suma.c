7f89eb8349c32b768cc8673f9a2487b3c4636433
Carlos <cdiezg07@estudiantes.unileon.es>
2019-02-14 14:09:55 +0100

    Suma de dos numeros

---
+++ suma.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+
+int num1,num2,suma;
+
+printf("Introduce un numero\n");
+scanf("%d",&num1);
+printf("Introduce el segundo numero\n");
+scanf("%d",&num2);
+
+suma=num1+num2;
+
+printf("La suma es ""%d + %d = %d\n", num1, num2, suma);
+
+
+
+
+
+return 0;
+}
