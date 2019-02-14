2982c7d02f927b1ca8716f4cd288f68e7dbdbe0f
Carlos <cdiezg07@estudiantes.unileon.es>
2019-02-14 15:03:21 +0100

    Convertir de kms a yardas, millas y pies.

--- convertir.c
+++ convertir.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-float km,millas,yardas,pies;
+float km=0,millas,yardas,pies;
 printf("Introduce una longitud en km\n");
 scanf("%f",&km);
 
@@ -10,7 +10,7 @@ millas=km/1.60;
 yardas=km*(100000/91.44);
 pies=km*(100000/30.48);
 
-printf("El valor en millas es " "%.3f" " en yardas es, " "%.3f" " y pies es " "%.3f\n" , millas, yardas, pies);
+printf("El valor en millas es %.3f en yardas es, %.3f y pies es %.3f\n", millas, yardas, pies);
 
 
 return 0;
